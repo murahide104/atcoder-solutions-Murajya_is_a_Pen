8ed4d76d0378b0294f5052c694cb220799e8fd55
Hidetoshi <hidetoshi0623@hotmail.com>
2023-04-15 19:01:51 +0900

    AC

--- ABC046_A.py
+++
@@ -1,3 +0,0 @@
-a, b, c = map(int, input().split())
-set_color = set([a,b,c])
-print(len(set_color))
--- ABC296_A.py
+++
@@ -1,6 +0,0 @@
-a, b = map(int, input().split())
-
-if b // 2 == a or a*2 == b or a*2+1 == b:
-  print("Yes")
-else:
-  print("No")
