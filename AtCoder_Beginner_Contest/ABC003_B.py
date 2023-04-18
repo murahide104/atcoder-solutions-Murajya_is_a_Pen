e5c7bb4111a033bed011e61dc57752d0047cfa88
Hidetoshi <hidetoshi0623@hotmail.com>
2023-04-18 10:22:23 +0900

    AC

---
+++ AtCoder_Beginner_Contest/ABC003_B.py
@@ -0,0 +1,31 @@
+s = input()
+t = input()
+at_list = ["a","t","c","o","d","e","r","@"]
+
+cnt_s = s.count("@")
+index_list = [-1]
+for i in range(cnt_s):
+    index_list.append(s.find("@", index_list[-1]+1))
+    if t[index_list[-1]] in at_list:
+        x = t[index_list[-1]]
+        s = s[:index_list[-1]]+x+s[index_list[-1]+1:]
+    else:
+        print("You will lose")
+        exit(0)
+
+cnt_t = t.count("@")
+index_list = [-1]
+for i in range(cnt_t):
+    index_list.append(t.find("@", index_list[-1]+1))
+    if s[index_list[-1]] in at_list:
+        x = s[index_list[-1]]
+        t = t[:index_list[-1]]+x+t[index_list[-1]+1:]
+    else:
+        print("You will lose")
+        exit(0)
+
+print(s+"\n"+t)
+if s == t:
+    print("You can win")
+else:
+    print("You will lose")
