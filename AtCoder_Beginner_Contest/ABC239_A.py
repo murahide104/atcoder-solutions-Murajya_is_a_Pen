99d130cfd3a70e72ebe80fb417da60d6584fd8e3
Hidetoshi <hidetoshi0623@hotmail.com>
2023-06-02 12:08:41 +0900

    AC

--- AtCoder_Beginner_Contest/ABC239_A.py
+++ AtCoder_Beginner_Contest/ABC239_A.py
@@ -1,4 +1,4 @@
 import math
 h = int(input())
 result = math.sqrt(h*(12800000+h))
-print(result) 
+print(result)
---
+++ AtCoder_Beginner_Contest/ABC246_A.py
@@ -0,0 +1,17 @@
+x1, y1 = map(int, input().split())
+x2, y2 = map(int, input().split())
+x3, y3 = map(int, input().split())
+
+x_list = [x1, x2, x3]
+y_list = [y1, y2, y3]
+
+for i in range(3):
+    if x_list.count(x_list[i]) == 1:
+        x = x_list[i]
+        break
+for i in range(3):
+    if y_list.count(y_list[i]) == 1:
+        y = y_list[i]
+        break
+
+print(x,y)
