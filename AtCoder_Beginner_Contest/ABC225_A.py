134b989e0922ff9a1f4e9fc8fec99e3268e3b236
Hidetoshi <hidetoshi0623@hotmail.com>
2023-07-23 16:24:56 +0900

    AC

---
+++ AtCoder_Beginner_Contest/ABC225_A.py
@@ -0,0 +1,7 @@
+s = set(list(input()))
+if len(s) == 3:
+    print(6)
+elif len(s) == 2:
+    print(3)
+else:
+    print(1)
--- AtCoder_Beginner_Contest/ABC310_A.py
+++ AtCoder_Beginner_Contest/ABC310_A.py
@@ -1,7 +1,7 @@
 n, p, q = map(int, input().split())
 l = list(map(int, input().split()))
 n = q + min(l)
-if p > n:
+if p < n:
     print(p)
 else:
     print(n)
