54cfcd03635cf78cc668cd7e8d7c8fe08ba95864
Hidetoshi <hidetoshi0623@hotmail.com>
2023-10-29 10:37:42 +0900

    AC

--- AtCoder_Beginner_Contest/ABC270_A.py
+++ AtCoder_Beginner_Contest/ABC270_A.py
@@ -1,19 +1,18 @@
 l = list(map(int, input().split()))
 set_list = set()
-for i in l:
-    if i == 1:
-        set_list.add(1)
-    elif i == 2:
-        set_list.add(2)
-    elif i == 3:
-        set_list.update([1,2])
-    elif i == 4:
-        set_list.add(4)
-    elif i == 5:
-        set_list.update([1,4])
-    elif i == 6:
-        set_list.update([2,4])
-    elif i == 7:
-        set_list.update([1,2,4])
 
+for i in l:
+    if 0 <= i - 4:
+        set_list.add(4)
+        i -= 4
+        if i == 0:break
+    if 0 <= i - 2:
+        set_list.add(2)
+        i -= 2
+        if i == 0:break
+    if 0 <= i - 1:
+        set_list.add(1)
+        i -= 0
+        if i == 0:break
+        
 print(sum(list(set_list)))
