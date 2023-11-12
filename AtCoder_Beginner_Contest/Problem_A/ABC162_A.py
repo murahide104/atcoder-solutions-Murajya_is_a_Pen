d67dd193b5a9cb392022a7c9ac22b1a61840839f
Hidetoshi <hidetoshi0623@hotmail.com>
2023-11-12 22:52:36 +0900

    AC

---
+++ AtCoder_Beginner_Contest/Problem_A/ABC162_A.py
@@ -0,0 +1,6 @@
+n = list(input())
+
+if "7" in n:
+    print("Yes")
+else:
+    print("No")
--- AtCoder_Beginner_Contest/Problem_A/ABC223_A.py
+++ AtCoder_Beginner_Contest/Problem_A/ABC223_A.py
@@ -1,5 +1,7 @@
 x = int(input())
-if x % 100 == 0:
+if x == 0:
+    print("No")
+elif x % 100 == 0:
     print("Yes")
 else:
     print("No")
