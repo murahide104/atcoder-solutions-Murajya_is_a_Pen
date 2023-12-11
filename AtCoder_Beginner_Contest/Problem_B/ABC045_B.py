94f96de6434e08df675eab1327d295be208d360a
Hidetoshi <hidetoshi0623@hotmail.com>
2023-12-11 15:58:02 +0900

    AC

---
+++ AtCoder_Beginner_Contest/Problem_B/ABC045_B.py
@@ -0,0 +1,24 @@
+a = list(input())
+b = list(input())
+c = list(input())
+
+check = "a"
+while True:
+    if check == "a":
+        if not a:
+            print("A")
+            break
+        else:
+            check = a.pop(0)
+    elif check == "b":
+        if not b:
+            print("B")
+            break
+        else:
+            check = c.pop(0)
+    else:
+        if not c:
+            print("C")
+            break
+        else:
+            check = c.pop(0)
