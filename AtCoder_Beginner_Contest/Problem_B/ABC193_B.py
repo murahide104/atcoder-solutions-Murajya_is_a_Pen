41b4643918dd7fcd07c300467f2e7a01fb796dbf
Hidetoshi <hidetoshi0623@hotmail.com>
2023-12-13 18:04:07 +0900

    AC

---
+++ AtCoder_Beginner_Contest/Problem_B/ABC193_B.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+cost = 10**10
+flag = 0
+
+for i in range(n):
+    a, p, x = map(int, input().split())
+    if x <= a: continue
+    if p < cost:
+        flag = 1
+        cost = p
+        
+print(cost if flag else -1)
+        
