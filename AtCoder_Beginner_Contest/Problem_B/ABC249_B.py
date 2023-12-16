7e6d7c9059a5273f5816e946dcaea5419dc82827
Hidetoshi <hidetoshi0623@hotmail.com>
2023-12-16 23:12:18 +0900

    AC^

---
+++ AtCoder_Beginner_Contest/Problem_B/ABC249_B.py
@@ -0,0 +1,18 @@
+s = input()
+
+u_check = True
+l_check = True
+unique_check = False
+
+for char in s:
+    if char.isupper():
+        u_check = False
+    if char.islower():
+        l_check = False
+    if s.count(char) != 1:
+        unique_check = True
+
+if u_check or l_check or unique_check:
+    print("No")
+else:
+    print("Yes")
