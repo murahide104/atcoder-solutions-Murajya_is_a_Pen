00ee183d18447c62deb1a67ade3c0bc8a5e7da4e
Hidetoshi <hidetoshi0623@hotmail.com>
2023-12-18 08:24:54 +0900

    AC

---
+++ AtCoder_Beginner_Contest/Problem_B/ABC283_B.py
@@ -0,0 +1,11 @@
+n = int(input())
+a = list(map(int, input().split()))
+q = int(input())
+
+for i in range(q):
+    l = list(map(int, input().split()))
+    
+    if l[0] == 1:
+        a[l[1]-1] = l[2]
+    else:
+        print(a[l[1]-1])
