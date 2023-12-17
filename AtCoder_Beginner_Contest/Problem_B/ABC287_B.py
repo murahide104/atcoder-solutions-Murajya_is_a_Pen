26257a0486f8453c38114be380ad5a1ceadf939d
Hidetoshi <hidetoshi0623@hotmail.com>
2023-12-17 13:56:32 +0900

    AC

---
+++ AtCoder_Beginner_Contest/Problem_B/ABC287_B.py
@@ -0,0 +1,16 @@
+n, m = map(int, input().split())
+s = []
+for i in range(n):
+    s.append(input()[3:])
+    
+t = []
+for i in range(m):
+    t.append(input())
+
+ans = 0
+for x in s:
+    if x in t:
+        ans += 1
+
+
+print(ans)
